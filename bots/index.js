072571a16128410290a199c5053c2b35caebb9b4
umarrg <umarabubakarfta@gmail.com>
2025-07-09 19:46:28 +0100

    added some fixes

--- bots/index.js
+++ bots/index.js
@@ -1,3 +1,4 @@
+import express from 'express';
 import 'dotenv/config';
 import { spawn } from 'child_process';
 import { fileURLToPath } from 'url';
@@ -16,7 +17,16 @@ const discordBot = spawn('node', [join(__dirname, 'discord-bot.js')], {
     stdio: 'inherit',
     shell: true
 });
+const app = express();
+const PORT = process.env.PORT || 9000;
 
+app.get('/', (req, res) => {
+    res.send('Quickvoicy Bots are running.');
+});
+
+app.listen(PORT, () => {
+    console.log(`✅ Express server is running at http://localhost:${PORT}`);
+});
 process.on('SIGINT', () => {
     console.log('\nShutting down bots...');
     telegramBot.kill();
